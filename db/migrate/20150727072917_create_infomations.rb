2967d818fffbb020431e39be3783dd7affeb1ea4
ichikawa <ichikawa@AirHiroki14.local>
2015-07-30 18:53:05 +0900

    DB修正

--- db/migrate/20150727072917_create_infomations.rb
+++ db/migrate/20150727072917_create_infomations.rb
@@ -2,7 +2,7 @@ class CreateInfomations < ActiveRecord::Migration
   def change
     create_table :infomations do |t|
       t.string :title, null:false
-      t.string :body, null:false
+      t.text :body, null:false
 
       t.timestamps null: false
     end
